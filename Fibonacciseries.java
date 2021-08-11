bf3d234f5b424087f6d1946b0f0fee0f9f00b5f7
Ajithkd <88609238+Ajithkd@users.noreply.github.com>
2021-08-11 19:47:51 +0530

    Add files via upload

---
+++ Fibonacciseries.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Fibonacciseries {
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		int range =8, firstNumber =0, secondNumber =1 ,sum;
+		System.out.println("Fibonacci series are :");
+		for (int i=1; i<=range; i++) {
+			System.out.println(+firstNumber);
+			sum = firstNumber + secondNumber;
+			firstNumber = secondNumber;
+			secondNumber = sum;
+			}
+		
+	}
+
+}
